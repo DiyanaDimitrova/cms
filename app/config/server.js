9ef92750b7bf0d9d31efd50f22ec969f929c27a2
Diyana Dimitrova <diyana.dimitrova.1992@gmail.com>
2022-01-26 22:23:53 +0200

    Initial commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e455d1da8c45e848ab960efee2628c4d'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '19f34ba2-d841-4088-9bc9-6bc38501c267'
+};
